30ec0e460a631953006a1ed22695e2a4f9e3849f
jalcacer <j.alcacer.dev@gmail.com>
2023-03-04 11:02:06 +0100

    Correccion de los  ; y eleccion del puerto

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,4 +2,12 @@
 const http = require('http');
 
 //--Importamos el módulo fs
-const fs = require('fs')
+const fs = require('fs');
+
+//--Importamos el modulo pathh
+const path = require('path');
+
+//elegimos el puerto desde el que el servidor va a escuchar (se pide el 9000)
+const PORT = 9000;
+
+
