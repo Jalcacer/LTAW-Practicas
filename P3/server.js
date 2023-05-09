12e3ab3a0494996b252d0d8deb2bba5528e44736
jalcacer <j.alcacer.dev@gmail.com>
2023-05-09 18:19:02 +0200

    inserto el puerto, express, el server html y el Websocket server

--- P3/server.js
+++ P3/server.js
@@ -4,3 +4,14 @@ const http = require('http');
 const express = require('express');
 const colors = require('colors');
 const fs = require('fs');
+
+const PUERTO = 9000;
+
+//-- Crear una nueva aplciacion web
+const app = express();
+
+//-- Crear un servidor, asociado a la App de express
+const server = http.Server(app);
+
+//-- Crear el servidor de websockets, asociado al servidor http
+const io = socket(server);
