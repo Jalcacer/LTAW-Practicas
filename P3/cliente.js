12e2f87372630e86f2d33666a16fbe9d116e6ff9
jalcacer <j.alcacer.dev@gmail.com>
2023-05-09 18:12:52 +0200

    creción del websocket

--- P3/cliente.js
+++ P3/cliente.js
@@ -5,3 +5,6 @@ const msg_entry = document.getElementById("msg_entry");
 const sendUser = document.getElementById("sendUser");
 const botonEnviar = document.getElementById("botonEnviar");
 const user_entry = document.getElementById("user_entry");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
