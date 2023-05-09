11cd0ca35828a219e898a5f643fa6f75c4685f01
jalcacer <j.alcacer.dev@gmail.com>
2023-05-09 18:12:27 +0200

    formulrio de envío de mensajes y elementos interfaz en el cliente.js

---
+++ P3/cliente.js
@@ -0,0 +1,7 @@
+//-- Elementos del interfaz
+const display = document.getElementById("display");
+const mensajesRecibidos = document.getElementById("mensajesRecibidos");
+const msg_entry = document.getElementById("msg_entry");
+const sendUser = document.getElementById("sendUser");
+const botonEnviar = document.getElementById("botonEnviar");
+const user_entry = document.getElementById("user_entry");
