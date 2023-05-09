5d3d428fae295b55a846d5f155ec36dd4e639250
jalcacer <j.alcacer.dev@gmail.com>
2023-05-09 18:53:31 +0200

    envío del mensaje

--- P3/cliente.js
+++ P3/cliente.js
@@ -18,3 +18,13 @@ socket.on("message", (msg)=>{
       display.innerHTML +=  "</p>" + msg.split("////")[0];
     }
   });
+
+
+//-- Al apretar el botón se envía un mensaje al servidor
+msg_entry.onchange = () => {
+    if (msg_entry.value)
+      socket.send(msg_entry.value);
+  
+    //-- Borrar el mensaje actual
+    msg_entry.value = "";
+  }
