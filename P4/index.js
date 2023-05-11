22acfd8f1172dfbb6dd501f505d5c1986ef54942
jalcacer <j.alcacer.dev@gmail.com>
2023-05-11 10:17:00 +0200

    llamo a electron y a los elementos de la interfaz

---
+++ P4/index.js
@@ -0,0 +1,18 @@
+// llamamos a electron 
+const electron = require('electron');
+
+console.log("Cargando aplicación..");
+
+//-- Obtener elementos de la interfaz
+const display = document.getElementById("display");
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+const info3 = document.getElementById("info3");
+const info4 = document.getElementById("info4");
+const info5 = document.getElementById("info5");
+const info6 = document.getElementById("info6");
+const info7 = document.getElementById("info7");
+const infoIP = document.getElementById("infoIP");
+const accesoChat = document.getElementById("accesoChat");
+const infoUSERS = document.getElementById("infoUSERS");
+const print = document.getElementById("print");
