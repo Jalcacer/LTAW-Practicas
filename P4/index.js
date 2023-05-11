e11d5badf7bd4bdf36682943529dbd12cc8344c8
jalcacer <j.alcacer.dev@gmail.com>
2023-05-11 10:44:40 +0200

    Acceso a las APIS por parte dle proceso de renderizado

--- P4/index.js
+++ P4/index.js
@@ -19,3 +19,14 @@ const print = document.getElementById("print");
 //mensaje de prueba 
 let msg_prueba = 1;
 infoUSERS.innerHTML = "0";
+
+//-- Acceder a la API de node para obtener la info
+//-- Sólo es posible si nos han dado permisos desde
+//-- el proceso princpal
+info1.textContent = process.versions.node;
+info2.textContent = process.versions.electron;
+info3.textContent = process.versions.chrome;
+info4.textContent = process.arch;
+info5.textContent = process.platform;
+info6.textContent = process.getSystemVersion();
+info7.textContent = process.type;
